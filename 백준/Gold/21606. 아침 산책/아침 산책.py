215e1a844d539fd27fae872c27576c92f8935403
ParkJJungmIn <61037158+ParkJJungmIn@users.noreply.github.com>
2023-10-25 21:23:09 +0900

    [Gold III] Title: 아침 산책, Time: 1208 ms, Memory: 91952 KB, Score: 165 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,44 +1,39 @@
+from collections import defaultdict
 import sys
 sys.setrecursionlimit(10**5)
 
+def DFS(v:int):
+    indoor = 0
+    for i in net[v]:
+        if A[i-1] == 0:
+            if i not in visited:
+                visited.add(i)
+                indoor += DFS(i)
+        else: indoor += 1
+
+    return indoor
+
 
 N = int(input())
-
-state_list = list(map(int ,list(input()) ))
-
-from collections import defaultdict
-graph = defaultdict(list)
-
-answer = 0
+A = list(map(int, list(input()))) # 장소 n에 대하여 A[n-1]이 1이면 실내, 0이면 실외.
+net = defaultdict(list)
+route_count = 0
 
 for _ in range(N-1):
-    a,b = map(int , input().split() )
+    u, v = map(int, sys.stdin.readline().split())
+    net[u].append(v)
+    net[v].append(u)
+    if A[u-1] == 1 and A[v-1] == 1:
+        route_count += 2 # 실내끼리 인접했을 경우 경로를 2개 더해준다.
 
-    graph[a-1].append(b-1)
-    graph[b-1].append(a-1)
+visited = set()
+for i in range(1, N+1):
+    indoor = 0
+    if A[i-1] == 0:
+        if i not in visited:
+            visited.add(i)
+            indoor = DFS(i)
 
-    if state_list[a-1] == 1 and state_list[b-1] == 1:
-        answer += 2
+    route_count += indoor*(indoor-1)
 
-visited = [0] * N
-
-
-def dfs(start):
-    count = 0
-    visited[start] = 1
-
-    for i in graph[start]:
-        if visited[i] == 0 and state_list[i] == 0:
-            count += dfs(i)
-        elif state_list[i] == 1:
-            count += 1
-    
-    return count
-
-
-for i in range(N):
-    if state_list[i] == 0 and visited[i] == 0:
-        tmp_cnt = dfs(i)
-        answer += (tmp_cnt) * (tmp_cnt-1)
-
-print(answer)
+print(route_count)
