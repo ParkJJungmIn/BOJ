02b1fb4b7df3490f1245bb3dd537e6edcc5e1a5c
ParkJJungmIn <61037158+ParkJJungmIn@users.noreply.github.com>
2023-10-25 21:17:37 +0900

    [Gold III] Title: 아침 산책, Time: 976 ms, Memory: 333916 KB, Score: 165 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,27 +1,44 @@
-N = int(input().strip())
-A = list(map(int, input().strip()))
-graph = [[] for _ in range(N)]
+import sys
+sys.setrecursionlimit(10**5)
+
+
+N = int(input())
+
+state_list = list(map(int ,list(input()) ))
+
+from collections import defaultdict
+graph = defaultdict(list)
+
+answer = 0
 
 for _ in range(N-1):
-    u, v = map(int, input().split())
-    graph[u-1].append(v-1)
-    graph[v-1].append(u-1)
+    a,b = map(int , input().split() )
 
-count = 0
+    graph[a-1].append(b-1)
+    graph[b-1].append(a-1)
 
-def dfs(curr, prev):
-    global count
+    if state_list[a-1] == 1 and state_list[b-1] == 1:
+        answer += 2
 
-    if A[curr] == 1 and curr != prev:
-        count += 1
-        return
+visited = [0] * N
+
+
+def dfs(start):
+    count = 0
+    visited[start] = 1
+
+    for i in graph[start]:
+        if visited[i] == 0 and state_list[i] == 0:
+            count += dfs(i)
+        elif state_list[i] == 1:
+            count += 1
     
-    for neighbor in graph[curr]:
-        if neighbor != prev:
-            dfs(neighbor, curr)
+    return count
+
 
 for i in range(N):
-    if A[i] == 1:
-        dfs(i, i)
+    if state_list[i] == 0 and visited[i] == 0:
+        tmp_cnt = dfs(i)
+        answer += (tmp_cnt) * (tmp_cnt-1)
 
-print(count)
+print(answer)
